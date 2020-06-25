bf018f09e61b617d9653b7543777707147507bb8
SimonTakman <simontakman@gmail.com>
2020-06-25 11:02:44 +0200

    refactored code

--- src/components/iconGrid/IconGrid.js
+++ src/components/iconGrid/IconGrid.js
@@ -5,21 +5,18 @@ import email from "../../icons/email.svg";
 import phone from "../../icons/phone.svg";
 import "./IconGrid.css";
 
-//Something does not work with evening the grid
 export default function IconGrid() {
   return (
     <div className="IconGrid">
       <a href="https://github.com/SimonTakman/">
         <img src={github} alt="github" />
       </a>
-
       <a href="https://linkedin.com/in/simontakman">
         <img src={linkedin} alt="linkedin" />
       </a>
       <a href="mailto:simon@takmantechnologies.com">
         <img src={email} alt="takmantechnologies" />
       </a>
-
       <a href="tel:+46703253698">
         <img src={phone} alt="phone" />
       </a>
